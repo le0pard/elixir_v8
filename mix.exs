b306f232710e065e5ad72f406703e7915ddda1ae
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-09-13 00:26:35 +0300

    stable elixir

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule ElixirV8.Mixfile do
 
   def project do
     [app: :elixir_v8,
-     version: "0.1.1",
-     elixir: "~> 0.15",
+     version: "0.2.0",
+     elixir: "~> 1.0",
      deps: deps]
   end
 
